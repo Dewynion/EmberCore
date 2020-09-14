abdfa1c85be66d45ffae934612b966ac6a009ed2
BuildTools <siochain.agus.saoirse@gmail.com>
2020-09-13 21:35:00 -0400

    Implemented config API to simplify some config operations + multiple custom configs for one plugin

--- src/main/java/com/github/Dewynion/embercore/EmberCore.java
+++ src/main/java/com/github/Dewynion/embercore/EmberCore.java
@@ -1,11 +1,16 @@
 package com.github.Dewynion.embercore;
 
+import com.github.Dewynion.embercore.config.ConfigReader;
+import com.github.Dewynion.embercore.gui.menu.MenuManager;
 import com.github.Dewynion.embercore.test.command.CommandMenutest;
 import com.github.Dewynion.embercore.test.command.CommandShape;
 import com.github.Dewynion.embercore.reflection.ReflectionHelper;
+import io.netty.handler.logging.LogLevel;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class EmberCore extends JavaPlugin {
     private static EmberCore instance;
@@ -14,15 +19,32 @@ public class EmberCore extends JavaPlugin {
         return instance;
     }
 
+    public static MenuManager getMenuManager() {
+        return MenuManager.getInstance();
+    }
+
+    public static ConfigReader getConfigReader() {
+        return ConfigReader.getInstance();
+    }
+
+    /**
+     * Shortcut for {@link EmberCore#getInstance()#getLogger()#log(Level, String)}.
+     * Less typing.
+     */
+    public static void log(Level level, String message) {
+        instance.getLogger().log(level, message);
+    }
+
     public void onEnable() {
         instance = this;
-        ReflectionHelper.getSingletons(this);
         ReflectionHelper.registerEvents(this);
-
-        File config = new File(getDataFolder(), "config.yml");
-        if (!config.exists())
-            saveDefaultConfig();
+        setup(this);
         registerCommands();
+    }
+
+    public void setup(JavaPlugin plugin) {
+        ReflectionHelper.registerEvents(plugin);
+        getConfigReader().registerPlugin(this);
     }
 
     private void registerCommands() {
